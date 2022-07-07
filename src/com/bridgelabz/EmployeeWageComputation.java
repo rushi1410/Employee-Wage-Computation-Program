1e94dce7b322316eeb8ba2ba01fda7e7a69e8b74
rushi1410 <mr.rushikesh.molkire@outlook.com>
2022-07-07 11:27:31 +0530

    Added:Calculate Part time Employee Wage Using Switch Case

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -7,18 +7,19 @@ public class EmployeeWageComputation {
         final int full_time = 2;
         final int WAGE_PER_HR = 20;
         int emptype = (int) (Math.random() * 100) % 3;
-        int workinghours = 0;
-        if (emptype == full_time)
+        int workinghours =0;
+        switch (emptype)
         {
-            System.out.println("Employee is Present Full time");
-            workinghours = 8;
-        } else if (emptype == part_time)
-        {
-            System.out.println("Employee is Present Part time");
-            workinghours = 4;
-        } else
-        {
-            System.out.println("Employee is Absent");
+            case full_time:
+                System.out.println("Employee is Present Full time");
+                workinghours = 8;
+                break;
+            case part_time:
+                System.out.println("Employee is Present Part time");
+                workinghours = 4;
+                break;
+            default:
+                System.out.println("Employee is Absent");    
         }
         int wage = workinghours * WAGE_PER_HR;
         System.out.println("Employee Daily Wage is " + wage);
